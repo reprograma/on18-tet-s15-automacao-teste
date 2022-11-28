4baae7821179252c35f3c2dd9049878092c0b323
MBTGrazielle <grazimelo72@gmail.com>
2022-11-28 11:46:00 -0300

    Revisei Autorização

--- projeto-casa/src/routes/pacienteRoutes.js
+++ projeto-casa/src/routes/pacienteRoutes.js
@@ -1,11 +1,13 @@
 const rotas = require("express").Router()
 const controller = require("../controllers/pacienteController")
 
-rotas.get("/:id", controller.buscarPacienteId)
-rotas.get("/", controller.buscarTodosPacientes)
-rotas.post("/", controller.criarPaciente)
-rotas.delete("/:id", controller.deletarPaciente)
-rotas.patch("/:id", controller.atualizarPaciente)
+const { checkAuth } = require("../middlewares/auth");
+
+rotas.get("/:id",checkAuth, controller.buscarPacienteId)
+rotas.get("/",checkAuth, controller.buscarTodosPacientes)
+rotas.post("/",checkAuth, controller.criarPaciente)
+rotas.delete("/:id",checkAuth, controller.deletarPaciente)
+rotas.patch("/:id",checkAuth, controller.atualizarPaciente)
 
 
 
