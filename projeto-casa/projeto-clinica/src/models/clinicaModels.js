40f5daba9e60e15da36c0ac08d9f8ad5bfdcdbb9
Vitória Jara <vitoria.a.jara@gmail.com>
2022-12-16 18:48:14 -0400

    primeiro commit

---
+++ projeto-casa/projeto-clinica/src/models/clinicaModels.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const pacienteSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Types.ObjectId,
+        default: mongoose.Types.ObjectId
+    },
+    nome: {
+        type: String,
+        required: true
+    },
+    telefone: {
+        type: String,
+        required: true
+    },
+    endereco: {
+        type: String,
+        required: true
+    },
+    plano_saude: {
+        type: String,
+        required: false
+    },
+    plano_saude_numero: {
+        type: Number,
+        required: false
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("paciente", pacienteSchema)
